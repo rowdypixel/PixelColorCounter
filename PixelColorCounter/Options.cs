3185ab073efd4d8a226a39a2ee0a0ee44fadba7e
Dan Walker <dan@rowdypixel.com>
2022-12-31 19:24:46 -0500

    Count pixels of specific color

---
+++ PixelColorCounter/Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommandLine;
+
+namespace PixelColorCounter
+{
+	public class Options
+	{
+        [Option(
+            Required = true,
+            HelpText = "The image file to process")]
+        public string InputFile { get; set; }
+
+        [Option(
+            Default = "ff0000",
+            Required = false,
+            HelpText = "A hex color code to look for in the file")]
+        public string Color { get; set; }
+    }
+}
+
--- PixelColorCounter/Program.cs
+++ PixelColorCounter/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.IO;
+using CommandLine;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
+namespace PixelColorCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(RunOptions)
+            .WithNotParsed(HandleParseError);
+            Console.ReadKey();
+        }
+
+        static void RunOptions(Options opts)
+        {
+            Console.WriteLine($"Scanning {opts.InputFile}");
+
+
+            var colorToTrack = Rgba32.ParseHex("ff0000");
+            long coloredPixels = 0;
+
+            using (var image = Image.Load<Rgba32>(opts.InputFile))
+            {
+                var total = image.Width * (long)image.Height;
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        if (image[x, y] == colorToTrack)
+                            coloredPixels++;
+
+                    }
+                }
+
+                var percentage = (decimal)coloredPixels / total;
+
+                Console.WriteLine($"Color Pixels: {coloredPixels:N0}");
+                Console.WriteLine($"Total Pixels: {total:N0}");
+                Console.WriteLine($"Color Percentage: {percentage:P}");
+            }
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine("Could not parse command line options");
+        }
+    }
+}
